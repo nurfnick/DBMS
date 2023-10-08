30126fdc11ece994d822c69b991effea02e917e5
nurfnick <njacob@ecok.edu>
2023-10-07 20:39:50 -0500

    have everything working for now...

--- Project/FinalProject.sql
+++ Project/FinalProject.sql
@@ -1,13 +1,16 @@
 -- While working on the database design, it's useful to start from scratch every time
 -- Hence, we drop tables in reverse order they are created (so the foreign keyconstraints are not violated)
+DROP TABLE IF EXISTS Enrollment
+DROP TABLE IF EXISTS Student
+DROP TABLE IF EXISTS Class
 DROP TABLE IF EXISTS Cut_Job;
 DROP TABLE IF EXISTS Paint_Job;
 DROP TABLE IF EXISTS Fit_Job;
 DROP TABLE IF EXISTS Costs;
-DROP TABLE IF EXISTS Transaction;
+DROP TABLE IF EXISTS Transact;
 DROP TABLE IF EXISTS Assign;
 DROP TABLE IF EXISTS Job;
-DROP TABLE IF EXISTS Maintain;
+DROP TABLE IF EXISTS Maintains;
 DROP TABLE IF EXISTS Account;
 DROP TABLE IF EXISTS Cut;
 DROP TABLE IF EXISTS Paint;
@@ -18,16 +21,16 @@ DROP TABLE IF EXISTS Orders;
 DROP TABLE IF EXISTS Customer;
 DROP TABLE IF EXISTS Manufacture;
 DROP TABLE IF EXISTS Assemblies;
-DROP TABLE IF EXISTS Process;
+DROP TABLE IF EXISTS Processes;
 -- Create tables
-CREATE TABLE Process(
+CREATE TABLE Processes(
 process_id INT PRIMARY KEY,
-process_data VARCHAR(64) NOT NULL
+process_data VARCHAR(64)
 );
 CREATE TABLE Assemblies(
 assembly_id INT PRIMARY KEY,
 date_ordered DATE,
-assembly_details VARCHAR(64) NOT NULL
+assembly_details VARCHAR(64)
 );
 CREATE TABLE Manufacture (
 process_id INT,
@@ -92,7 +95,7 @@ CONSTRAINT PK_maintain PRIMARY KEY(acct_id,dept_num,process_id,assembly_id),
 CONSTRAINT FK_maintain_process FOREIGN KEY(process_id) REFERENCES Process,
 CONSTRAINT FK_maintain_acct FOREIGN KEY(acct_id) REFERENCES Account,
 CONSTRAINT FK_maintain_department FOREIGN KEY(dept_num) REFERENCES Department,
-CONSTRAINT FK_maintain_assembly FOREIGN KEY(assembly_id) REFERENCES Assembly
+CONSTRAINT FK_maintain_assembly FOREIGN KEY(assembly_id) REFERENCES Assemblies
 );
 CREATE TABLE Job(
 job_num INT PRIMARY KEY,
@@ -106,9 +109,9 @@ process_id INT,
 CONSTRAINT PK_assign PRIMARY KEY(job_num,process_id,assembly_id),
 CONSTRAINT FK_assign_process FOREIGN KEY(process_id) REFERENCES Process,
 CONSTRAINT FK_assign_job FOREIGN KEY(job_num) REFERENCES Job,
-CONSTRAINT FK_assign_assembly FOREIGN KEY(assembly_id) REFERENCES Assembly
+CONSTRAINT FK_assign_assembly FOREIGN KEY(assembly_id) REFERENCES Assemblies
 );
-CREATE TABLE Transaction(
+CREATE TABLE Transact(
 tran_num INT PRIMARY KEY,
 sup_cost INT
 );
@@ -123,8 +126,8 @@ CONSTRAINT PK_Costs PRIMARY KEY(job_num,acct_id,dept_num,process_id,assembly_id,
 CONSTRAINT FK_cost_process FOREIGN KEY(process_id) REFERENCES Process,
 CONSTRAINT FK_cost_acct FOREIGN KEY(acct_id) REFERENCES Account,
 CONSTRAINT FK_cost_department FOREIGN KEY(dept_num) REFERENCES Department,
-CONSTRAINT FK_cost_assembly FOREIGN KEY(assembly_id) REFERENCES Assembly,
-CONSTRAINT FK_cost_transation FOREIGN KEY(tran_num) REFERENCES Transaction,
+CONSTRAINT FK_cost_assembly FOREIGN KEY(assembly_id) REFERENCES Assemblies,
+CONSTRAINT FK_cost_transact FOREIGN KEY(tran_num) REFERENCES Transact,
 CONSTRAINT FK_cost_job FOREIGN KEY(job_num) REFERENCES Job
 );
 CREATE TABLE Fit_Job(
@@ -147,3 +150,60 @@ material NUMERIC(2,2),
 labor NUMERIC(3,0),
 CONSTRAINT FK_cut_job FOREIGN KEY(job_num) REFERENCES Job
 );
+
+DROP PROCEDURE IF EXISTS query1 --get rid of the procedure if you built it before
+
+GO
+CREATE PROCEDURE query1 --this is the first.  Need three inputs
+	@name VARCHAR(64),
+    @address VARCHAR(64),
+    @category NUMERIC(2,0)
+AS
+BEGIN
+	INSERT INTO Customer VALUES (@name, @address, @category) --insert me now
+END
+GO
+DROP PROCEDURE IF EXISTS query2 --get rid of the procedure if you built it before
+
+GO
+CREATE PROCEDURE query2 
+	@dept_num INT,
+    @dept_data VARCHAR(128)
+AS
+BEGIN
+	INSERT INTO Department VALUES (@dept_num, @dept_data) --insert me now
+END
+GO
+DROP PROCEDURE IF EXISTS query3 --get rid of the procedure if you built it before
+
+GO
+CREATE PROCEDURE query3 --this is the first.  Need three inputs
+	@process_id INT,
+    @process_data VARCHAR(64),
+    @type VARCHAR(5),
+    @type_type VARCHAR(64),
+    @type_method VARCHAR(64)
+AS
+BEGIN
+	INSERT INTO Process VALUES (@process_id, @process_data) --insert into process
+	IF @type = 'Fit' INSERT INTO Fit VALUES (@process_id, @type_type)
+	IF @type = 'Paint' INSERT INTO Paint VALUES (@process_id, @type_type, @type_method)
+	IF @type = 'Cut' INSERT INTO Cut VALUES(@process_id, @type_type, @type_method)
+END
+GO
+DROP PROCEDURE IF EXISTS query4 --get rid of the procedure if you built it before
+
+GO
+CREATE PROCEDURE query4 --this is the first.  Need three inputs
+	@assembly_id INT,
+    @date_ordered DATE,
+    @assembly_details VARCHAR(64),
+    @name VARCHAR(64),
+    @process_ids VARCHAR(64)--take this as a string seperated by commas and we'll slice it up
+AS
+BEGIN
+	INSERT INTO Assemblies VALUES (@assembly_id, @date_ordered, @assembly_details) --insert into assemblies
+	INSERT INTO Orders VALUES (@name,@assembly_id) --record what customer made the order
+	--INSERT INTO Manufactures (process_id,assembly_id) SELECT *,@assembly_id FROM (SELECT value FROM STRING_SPLIT(@process_ids,','))
+END
+GO
